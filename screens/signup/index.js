f88aa1438bffe781f37193633e4633105643db73
Jason Brown <browniefed@gmail.com>
2016-12-16 19:56:05 -0800

    Fix up correct styling

--- screens/signup/index.js
+++ screens/signup/index.js
@@ -11,8 +11,6 @@ import {
   TouchableOpacity
 } from 'react-native'
 
-const { width, height } = Dimensions.get("window");
-
 const background = require("./signup_bg.png");
 const backIcon = require("./back.png");
 const personIcon = require("./signup_person.png");
@@ -24,66 +22,108 @@ export default class SignupVriew extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Image source={background} style={styles.bg} resizeMode="cover" >
-          <View style={styles.headerIconView}>
-            <TouchableOpacity style={styles.headerBackButtonView}>
-              <Image source={backIcon} style={styles.backButtonIcon} />
-            </TouchableOpacity>
+        <Image 
+          source={background} 
+          style={[styles.container, styles.bg]}
+          resizeMode="cover"
+        >
+          <View style={styles.headerContainer}>
+
+            <View style={styles.headerIconView}>
+              <TouchableOpacity style={styles.headerBackButtonView}>
+                <Image 
+                  source={backIcon} 
+                  style={styles.backButtonIcon} 
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.headerTitleView}>
+              <Text style={styles.titleViewText}>Sign Up</Text>
+            </View>
+
           </View>
-          <View style={styles.headerTitleView}>
-            <Text style={styles.titleViewText}>Sign Up</Text>
-          </View>
-          <View style={styles.inputs}>
+
+          <View style={styles.inputsContainer}>
+
             <View style={styles.inputContainer}>
-              <View style={styles.imageContainer}>
-                <Image source={personIcon} style={styles.inputIcon} />
+              <View style={styles.iconContainer}>
+                <Image 
+                  source={personIcon}
+                  style={styles.inputIcon}
+                  resizeMode="contain"
+                />
               </View>
               <TextInput
                 style={[styles.input, styles.whiteFont]}
                 placeholder="Name"
                 placeholderTextColor="#FFF"
-                underlineColorAndroid='transparent' />
+                underlineColorAndroid='transparent' 
+              />
             </View>
+
             <View style={styles.inputContainer}>
-              <View style={styles.imageContainer}>
-                <Image source={emailIcon} style={styles.inputIcon} />
+              <View style={styles.iconContainer}>
+                <Image 
+                  source={emailIcon} 
+                  style={styles.inputIcon} 
+                  resizeMode="contain"
+                />
               </View>
               <TextInput
                 style={[styles.input, styles.whiteFont]}
                 placeholder="Email"
-                placeholderTextColor="#FFF" />
+                placeholderTextColor="#FFF" 
+              />
             </View>
+
             <View style={styles.inputContainer}>
-              <View style={styles.imageContainer}>
-                <Image source={lockIcon} style={styles.inputIcon} />
+              <View style={styles.iconContainer}>
+                <Image 
+                  source={lockIcon} 
+                  style={styles.inputIcon} 
+                  resizeMode="contain"
+                />
               </View>
               <TextInput
                 secureTextEntry={true}
                 style={[styles.input, styles.whiteFont]}
                 placeholder="Password"
-                placeholderTextColor="#FFF" />
+                placeholderTextColor="#FFF" 
+              />
             </View>
+
             <View style={styles.inputContainer}>
-              <View style={styles.imageContainer}>
-                <Image source={birthdayIcon} style={styles.inputIcon} />
+              <View style={styles.iconContainer}>
+                <Image 
+                  source={birthdayIcon} 
+                  style={styles.inputIcon} 
+                  resizeMode="contain"
+                />
               </View>
               <TextInput
                 style={[styles.input, styles.whiteFont]}
                 placeholder="Birthday"
                 placeholderTextColor="#FFF"
-                underlineColorAndroid='transparent' />
+                underlineColorAndroid='transparent' 
+              />
             </View>
+
           </View>
+
           <View style={styles.footerContainer}>
-            <View style={styles.signupView}>
-              <TouchableOpacity
-                style={styles.signup}>
-                <Text style={styles.signupText}>Join</Text>
-              </TouchableOpacity>
-            </View>
-            <TouchableOpacity
-              style={styles.signin}>
-              <Text style={styles.greyFont}>Already have an account?<Text style={styles.whiteFont}> Sign In</Text></Text>
+
+            <TouchableOpacity>
+              <View style={styles.signup}>
+                <Text style={styles.whiteFont}>Join</Text>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+              <View style={styles.signin}>
+                <Text style={styles.greyFont}>Already have an account?<Text style={styles.whiteFont}> Sign In</Text></Text>
+              </View>
             </TouchableOpacity>
           </View>
         </Image>
@@ -97,103 +137,75 @@ let styles = StyleSheet.create({
     flex: 1,
   },
   bg: {
-    width,
-    height,
+    paddingTop: 30,
+    width: null,
+    height: null
+  },
+  headerContainer: {
+    flex: 1,
+  },
+  inputsContainer: {
+    flex: 3,
+    marginVertical: 50,
+  },
+  footerContainer: {
+    flex: 1
   },
   headerIconView: {
-    flex: 1,
+    marginLeft: 10,
     backgroundColor: 'transparent'
   },
   headerBackButtonView: {
     width: 25,
     height: 25,
-    position: 'absolute',
-    top: 35,
-    left: 15,
   },
   backButtonIcon: {
-    resizeMode: 'contain',
     width: 25,
     height: 25
   },
   headerTitleView: {
-    flex: 1,
     backgroundColor: 'transparent',
-    paddingLeft: 25,
+    marginTop: 25,
+    marginLeft: 25,
   },
   titleViewText: {
     fontSize: 40,
     color: '#fff',
   },
-  errorText: {
-    color: '#FF3366',
-    paddingBottom: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    alignSelf: 'center',
-    fontSize: 15,
-  },
   inputs: {
-    paddingTop: 20,
-    paddingBottom: 10,
-    flex: 4
+    paddingVertical: 20,
   },
-  footerContainer: {
-    flex: 2,
+  inputContainer: {
+    borderWidth: 1,
+    borderBottomColor: '#CCC',
+    borderColor: 'transparent',
+    flexDirection: 'row',
+    height: 75,
+  },
+  iconContainer: {
+    paddingHorizontal: 15,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  signupView: {
-    paddingTop: 20,
+  inputIcon: {
+    width: 30,
+    height: 30,
+  },
+  input: {
+    flex: 1,
+    fontSize: 20,
   },
   signup: {
     backgroundColor: '#FF3366',
-    height: 50,
-    width: Dimensions.get('window').width,
+    paddingVertical: 25,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  signupText: {
-    color: '#FFF',
+    marginBottom: 15,
   },
   signin: {
     justifyContent: 'center',
     alignItems: 'center',
-    flex: 1,
     backgroundColor: 'transparent',
-  },
-  inputIcon: {
-    width: 25,
-    height: 25,
-  },
-  imageContainer: {
-    paddingLeft: 20,
-    paddingRight: 10,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  inputContainer: {
-    borderWidth: 1,
-    paddingBottom: 10,
-    borderBottomColor: '#CCC',
-    borderColor: 'transparent',
-    flexDirection: 'row'
-  },
-  input: {
-    height: 50,
-    flex: 10,
-    paddingLeft: 20,
-    fontSize: 22,
-  },
-  forgotContainer: {
-    paddingTop:10,
-    paddingRight: 10,
-  },
-  forgotText: {
-    fontSize: 13,
-    alignSelf: 'flex-end',
-    color: '#D8D8D8',
   },
   greyFont: {
     color: '#D8D8D8'
